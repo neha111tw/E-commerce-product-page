23a6e9a6f9634fcff0a7a734827c5d4219cc08ce
neha111tw <neha.tw7@gmail.com>
2025-04-15 18:18:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
             />
           </div>
           <div>
-            <input
+            <Input
               type="text"
               placeholder="Search..."
               value={searchItem}
